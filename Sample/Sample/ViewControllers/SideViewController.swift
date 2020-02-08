b195cdad7da66dbcc7a49a9afa2831e6ea733d7b
Taharcca <39498829+Taharcca@users.noreply.github.com>
2020-02-08 22:20:05 +0100

    I want to enable the navigation bars for every View Controller embedded in the NavigationController inside the Tab Bar Controller

---
+++ Sample/Sample/ViewControllers/SideViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SideViewController.swift
+//  Sample
+//
+//  Created by  on 08.02.20.
+//  Copyright © 2020 . All rights reserved.
+//
+
+import UIKit
+
+class SideViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Search"
+
+    }
+    @objc func action(){
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
